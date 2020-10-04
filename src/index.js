bbb51d7bf2e506272683d16f5c3df894c789240d
grachevrv <grachevrv@ya.ru>
2020-10-04 17:22:35 +0400

    feat: with 2 errors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [],
+  open = [],
+  close = [],
+  itsOpen = true
+
+bracketsConfig.forEach(element => {
+  open.push(element[0])
+  close.push(element[1])
+});
+
+
+
+for (let i = 0; i < str.length; i++) {
+
+  if (open.indexOf(str[i]) == close.indexOf(str[i])) {
+    if (itsOpen) {
+      stack.push(str[i])
+      itsOpen = false
+    } else {
+      if (close.indexOf(str[i]) != -1) {
+        if (close.indexOf(str[i]) != open.indexOf(stack.pop())) {
+          return false
+        }
+        itsOpen = true
+      }
+    }
+  } else {
+    if (open.indexOf(str[i]) != -1) {
+      stack.push(str[i])
+    }
+    if (close.indexOf(str[i]) != -1) {
+
+      if (close.indexOf(str[i]) != open.indexOf(stack.pop())) {
+        return false
+      }
+    }
+  }
+}
+
+if (stack.length !== 0) {
+  return false;
+}
+
+return true;
 }
